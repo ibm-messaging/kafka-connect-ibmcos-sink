c163fc0c5ac4b76c202c48c9ba9c0d6e7d5873a2
Adrian Preston <prestona@uk.ibm.com>
2018-12-28 22:06:28 +0000

    Add comments to PartitionWriter and factory
    Clarify the contract for being a PartitionWriter by commenting
    this interface.

--- src/main/java/com/ibm/eventstreams/connect/cossink/partitionwriter/PartitionWriter.java
+++ src/main/java/com/ibm/eventstreams/connect/cossink/partitionwriter/PartitionWriter.java
@@ -2,13 +2,28 @@ package com.ibm.eventstreams.connect.cossink.partitionwriter;
 
 import org.apache.kafka.connect.sink.SinkRecord;
 
+/**
+ * PartitionWriter instances writes {@code SinkRecords} for one Kafka partition
+ * to IBM COS. An instance is created at the point a partition is assigned to
+ * a SinkTask and is closed when the partition is no longer assigned to the task.
+ */
 public interface PartitionWriter {
 
-    // TODO: how best to avoid re-committing the same offset if no progress has been
-    //       made between calls?
+    /**
+     * preCommit is called just before the connector commits offsets for this partition.
+     * @return the offset to commit for this partition, or <code>null</code> if the offset
+     *         hasn't changed since the last time {@code #preCommit()} was called.
+     */
     Long preCommit();
 
+    /**
+     * put passes a {@code SinkRecord} to this writer for processing.
+     * @param record
+     */
     void put(SinkRecord record);
 
+    /**
+     * close is called when the writer is no longer required.
+     */
     void close();
 }
--- src/main/java/com/ibm/eventstreams/connect/cossink/partitionwriter/PartitionWriterFactory.java
+++ src/main/java/com/ibm/eventstreams/connect/cossink/partitionwriter/PartitionWriterFactory.java
@@ -3,5 +3,11 @@ package com.ibm.eventstreams.connect.cossink.partitionwriter;
 import com.ibm.cos.Bucket;
 
 public interface PartitionWriterFactory {
+
+    /**
+     * Creates a new {@code PartitionWriter} instance
+     * @param bucket the COS bucket that the writer will write into.
+     * @return
+     */
     PartitionWriter newPartitionWriter(Bucket bucket);
 }
